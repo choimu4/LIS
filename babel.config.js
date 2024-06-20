cdcd3b16a571db6b26f7e6004a582a57e61fc02d
Minuk Choi <choimu4@naver.com>
2024-06-20 14:20:26 +0900

    12333123

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    presets: ["@babel/preset-env"],
+    presets: [
+      "@babel/preset-env",
+      "@babel/preset-react"
+    ],
     plugins: [
       "@babel/plugin-proposal-class-properties",
       "@babel/plugin-proposal-private-methods"
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,55 @@
-const { override, addBabelPreset, addBabelPlugin } = require('customize-cra');
+const path = require('path');
 
-module.exports = override(
-  addBabelPreset('@babel/preset-env'),
-  addBabelPlugin('@babel/plugin-proposal-class-properties'),
-  addBabelPlugin('@babel/plugin-proposal-private-methods'),
-  addBabelPlugin('@babel/plugin-proposal-private-property-in-object'),
-  (config) => {
-    // Ensure that mjs files are processed with babel-loader
-    const oneOfRule = config.module.rules.find(rule => rule.oneOf);
-    if (oneOfRule) {
-      oneOfRule.oneOf.unshift({
-        test: /\.mjs$/,
-        include: /node_modules/,
-        type: 'javascript/auto',
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, 'node_modules/mysql2'),
+        ],
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
             plugins: [
               '@babel/plugin-proposal-class-properties',
-              '@babel/plugin-proposal-private-methods',
-              '@babel/plugin-proposal-private-property-in-object'
+              '@babel/plugin-proposal-private-methods'
             ]
           }
         }
-      });
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    fallback: {
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "path": require.resolve("path-browserify"),
+      "fs": false,
+      "net": false,
+      "tls": false,
+      "zlib": require.resolve("browserify-zlib"),
+      "timers": require.resolve("timers-browserify"),
+      "http": require.resolve("stream-http"),
+      "querystring": require.resolve("querystring-es3"),
+      "async_hooks": false,
+      "vm": require.resolve("vm-browserify")
     }
-    return config;
+  },
+  performance: {
+    hints: false
   }
-);
+};
